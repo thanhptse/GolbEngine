b22325e7de7e0a3e77962dc877c570964e13cf63
Thanh Phan Trung <thanh.phantrung@nashtechglobal.com>
2018-10-22 18:04:48 +0700

    update dbcontext and base repository

--- GolbEngine.Data/EntityFramework/AppDbContext.cs
+++ GolbEngine.Data/EntityFramework/AppDbContext.cs
@@ -1,17 +1,44 @@
 ﻿using GolbEngine.Data.Entities;
+using GolbEngine.Data.Interfaces;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace GolbEngine.Data.EntityFramework
 {
-    public class AppDbContext
+    public class AppDbContext : IdentityDbContext<AppUser, AppRole, Guid>
     {
         public DbSet<Blog> Blog { get; set; }
         public DbSet<BlogTag> BlogTag { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Tag> Tags { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+        public override int SaveChanges()
+        {
+            var modified = ChangeTracker.Entries().Where(e => e.State == EntityState.Modified || e.State == EntityState.Added);
+
+            foreach (EntityEntry item in modified)
+            {
+                var changedOrAddedItem = item.Entity as IDateTracking;
+                if (changedOrAddedItem != null)
+                {
+                    if (item.State == EntityState.Added)
+                    {
+                        changedOrAddedItem.DateCreated = DateTime.Now;
+                    }
+                    changedOrAddedItem.DateModified = DateTime.Now;
+                }
+            }
+            return base.SaveChanges();
+        }
     }
 }
---
+++ GolbEngine.Data/EntityFramework/EFUnitOfWork.cs
@@ -0,0 +1,26 @@
+﻿using GolbEngine.Infrastructure.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GolbEngine.Data.EntityFramework
+{
+    public class EFUnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+        public EFUnitOfWork(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
