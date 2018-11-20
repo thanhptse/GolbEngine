74954f1ac6e4ad6a14d714108c426419bded1ff6
Thanh Phan <thanhptse@gmail.com>
2018-11-20 08:56:57 +0700

    update some page in user area

--- GolbEngine/Controllers/HomeController.cs
+++ GolbEngine/Controllers/HomeController.cs
@@ -39,5 +39,10 @@ namespace GolbEngine.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Course()
+        {
+            return View();
+        }
     }
 }
---
+++ GolbEngine/Controllers/PostController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GolbEngine.Controllers
+{
+    public class PostController : Controller
+    {
+        public IActionResult Detail()
+        {
+            return View();
+        }
+
+        public IActionResult GoodPost()
+        {
+            return View();
+        }
+    }
+}
